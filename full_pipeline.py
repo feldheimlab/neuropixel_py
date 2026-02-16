ee1dcdb5d51c8d0e56ca5cfc54e73b0c9ae4065f
Brian Mullen <brmullen@ucsc.edu>
2026-02-16 10:06:00 -0800

    created check for filtered data and kilosort run

--- full_pipeline.py
+++ full_pipeline.py
@@ -28,6 +28,15 @@ sys.path.append('./python')
 from concatenate_data import get_file_org
 from config import configs
 
+def ask_yes_or_no(msg: str) -> bool:
+    while True:
+        if (user := input(msg).lower()) in ('y', 'yes'):
+            return True
+        if user in ('n' ,'no'):
+            return False
+        print('Invalid input. Please try again...')
+
+
 # List of scripts to run in order
 if __name__ == '__main__':
 
@@ -96,8 +105,35 @@ if __name__ == '__main__':
 	
 	config = configs(dataloc, saveloc, probe, n_datasets, fps)
 
-	print('Starting pipeline:')
+	walkresults = list(os.walk(config.saveloc))
+	try:
+		subdir = walkresults[0][1][0]
+		subdir_files = walkresults[1][2]
+		for file in subdir_files:
+			if file.endswith('.bin'):
+				binfile = file
+		config.binloc = os.path.join(config.saveloc, os.path.join(subdir, binfile))
+		kilosortloc = os.path.join(saveloc, os.path.join(subdir, 'kilosort4'))
+	except:
+		config.binloc = None
+		kilosortloc = None
+
+	if os.path.exists(config.binloc):
+		print('Found concatenated bin file:', config.binloc)
+		if not ask_yes_or_no('Would you like to redo the filter and concatenation step of the data processing? [Y/N]: '):
+			config.scripts_to_run.remove('filter_concatenate')
+	else:
+		print('No concatenated binfile has been found.')
 	
+	if os.path.exists(kilosortloc):
+		print('Found kilsort file:', kilosortloc)
+		if not ask_yes_or_no('Would you like to redo spikesorting with kilsort? [Y/N]: '):
+			config.scripts_to_run.remove('kilosort')
+	else:
+		print('No kilosort folder has been found.')	
+
+	print('Starting pipeline:')
+
 	# Run each script sequentially
 	for script in config.scripts_to_run:
 		print(f"--- Running {script} ---")
@@ -107,34 +143,53 @@ if __name__ == '__main__':
 			p = subprocess.Popen(command_dict['command'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
 			output, errors = p.communicate()
 			p.wait()
+			walkresults = list(os.walk(config.saveloc))
+			subdir = walkresults[0][1][0]
+			subdir_files = walkresults[1][2]
+			for file in subdir_files:
+				if file.endswith('.bin'):
+					binfile = file
+			config.binloc = os.path.join(config.saveloc, os.path.join(subdir, binfile))
+			
+			logloc = os.path.join(config.saveloc, 'CatGT.log') # move log file over to filtered location
+			if  os.path.exists(logloc):
+				print('\tDeleting log from previous run {}'.format(logloc))
+				os.remove(logloc)
+			
+			if os.path.exists(os.path.join(config.catGTwin_loc, 'CatGT.log')):
+				shutil.move(os.path.join(config.catGTwin_loc, 'CatGT.log'), logloc)
+			
+			assert os.path.exists(config.binloc), 'Could not find the binary file: {}'.format(config.binloc) # find the filtered binary file
+			print('\tFiltered data create at {}'.format(config.binloc))
+			
+		if 'filter_concatenate' not in config.scripts_to_run:	
+			walkresults = list(os.walk(config.saveloc))
+			subdir = walkresults[0][1][0]
+			subdir_files = walkresults[1][2]
+			for file in subdir_files:
+				if file.endswith('.bin'):
+					binfile = file
+			config.binloc = os.path.join(config.saveloc, os.path.join(subdir, binfile))
 
-		walkresults = list(os.walk(config.saveloc))
-		subdir = walkresults[0][1][0]
-		subdir_files = walkresults[1][2]
-		for file in subdir_files:
-			if file.endswith('.bin'):
-				binfile = file
-		config.binloc = os.path.join(config.saveloc, os.path.join(subdir, binfile))
-		
-		logloc = os.path.join(config.saveloc, 'CatGT.log') # move log file over to filtered location
-		if  os.path.exists(logloc):
-			print('\tDeleting log from previous run {}'.format(logloc))
-			os.remove(logloc)
-		
-		if os.path.exists(os.path.join(config.catGTwin_loc, 'CatGT.log')):
-			shutil.move(os.path.join(config.catGTwin_loc, 'CatGT.log'), logloc)
-		assert os.path.exists(config.binloc), 'Could not find the binary file: {}'.format(config.binloc) # find the filtered binary file
-		print('\tFiltered data create at {}'.format(config.binloc))
+			assert os.path.exists(config.binloc), 'Could not find the binary file: {}'.format(config.binloc) # find the filtered binary file
+			print('\tFiltered data located at {}'.format(config.binloc))
 
 		if script == 'kilosort':
+			kilosortloc = os.path.join(saveloc, os.path.join(subdir, 'kilosort4'))
+
 			script_dict = config.batch_script_to_run[script]
 			probe = kilosort.io.load_probe(script_dict['probe_loc'])
 			print('Loading probe from {}'.format(script_dict['probe_loc']))
 
 			kilosort.run_kilosort(script_dict['settings'], probe=probe, filename=config.binloc) 
-		kilosortloc = os.path.join(saveloc, os.path.join(subdir, 'kilosort4'))
-		assert os.path.exists(kilosortloc), 'Could not find the kilosort output files: {}'.format(kilosortloc) # find the filtered binary file
 
+			kilosortloc = os.path.join(saveloc, os.path.join(subdir, 'kilosort4'))
+			assert os.path.exists(kilosortloc), 'Could not find the kilosort output files: {}'.format(kilosortloc) # find the filtered binary file
+
+		if 'kilosort' not in config.scripts_to_run:
+			kilosortloc = os.path.join(saveloc, os.path.join(subdir, 'kilosort4'))
+			assert os.path.exists(kilosortloc), 'Could not find the kilosort output files: {}'.format(kilosortloc) # find the filtered binary file
+		
 		if script == 'waveform_classifier':
 			script_dict = config.batch_script_to_run[script]
 			for pyscript in  script_dict['scripts']:
@@ -143,6 +198,7 @@ if __name__ == '__main__':
 				if result.returncode != 0:
 					print(f"Error: {script} failed. Stopping sequence.")
 					break
+		
 		if script == 'TTL_generate':
 			script_dict = config.batch_script_to_run[script]
 			print(script_dict['script'])
@@ -151,5 +207,6 @@ if __name__ == '__main__':
 			if result.returncode != 0:
 				print(f"Error: {script} failed. Stopping sequence.")
 				break
+		
 		print(f"--- Finished {script} ---\n")
 		config.write_attributes()
